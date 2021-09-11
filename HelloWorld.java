bccfa2cc7324448dba053a5e77ea5f76cecbd582
robertorai <raimondi.roberto@gmail.com>
2021-09-11 18:08:22 +0200

    Test compilazione

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
-public static void main(String[] args){
-System.out.println("Ciao Ray!");
-}
+    public static void main(String[] args){
+        System.out.println("Ciao Ray!");
+    }
 }

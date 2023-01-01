d0cdd7f4ead9825a8d454cf343583b970509d1af
robertorai <raimondi.roberto@gmail.com>
2023-01-01 18:15:58 +0100

    test

--- Action.java
+++ Action.java
@@ -3,7 +3,7 @@ import java.util.Random;
 class Action {
   public static void main(String... args) {
     System.out.printf("Hello %s.%n", args.length == 0 ? "Java" : args[0]);
-    var gaussian = new Random().nextGaussian();
+    Double gaussian = new Random().nextGaussian();
     System.out.println("::set-output name=random-number::" + gaussian);
     System.out.println("Goodbye and have fun with: " + gaussian);
   }
